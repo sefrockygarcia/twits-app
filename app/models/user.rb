738b709c9f5010063c43e1f766ce3ed16c67f5c3
Frocky Garcia <se.frocky@gmail.com>
2022-01-05 09:40:30 +0800

    Integrate user registration and login

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+end
